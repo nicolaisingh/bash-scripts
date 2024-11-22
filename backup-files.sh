e2ba82bf8b2335f07adea150769fabce00b9e877
Nicolai Singh <nicolaisingh@pm.me>
2024-11-22 15:54:05 +0800

    updates

--- backup-files.sh
+++ backup-files.sh
@@ -1,37 +1,41 @@
 #!/usr/bin/env bash
 
-rsyncopts="-ravvh --progress --log-file=${HOME}/backup-files.log"
+rsyncopts="-ravh --info=NAME1 --progress --log-file=${HOME}/backup-files.log"
 
 echo '' > ${HOME}/backup-files.log
 
-dest=/run/media/USER/EXTERNAL_HD1
-if [ -d $dest ]; then
-	rsync ${rsyncopts} ${HOME}/calibre/ ${dest}/calibre
-	rsync ${rsyncopts} ${HOME}/configs/ ${dest}/configs
-	rsync ${rsyncopts} ${HOME}/dev/ ${dest}/dev
-	rsync ${rsyncopts} ${HOME}/Documents/ ${dest}/doc
-	rsync ${rsyncopts} ${HOME}/games/ ${dest}/games
-	rsync ${rsyncopts} ${HOME}/Pictures/ ${dest}/img
-	rsync ${rsyncopts} ${HOME}/Music/library/ ${dest}/music
-	rsync ${rsyncopts} ${HOME}/Music/sfx/ ${dest}/music-sfx
-	rsync ${rsyncopts} ${HOME}/Music/flac/ ${dest}/music-flac
-	rsync ${rsyncopts} ${HOME}/org/ ${dest}/org
-	rsync ${rsyncopts} ${HOME}/prj/ ${dest}/prj
-	rsync ${rsyncopts} ${HOME}/sw/ ${dest}/sw
+dest="${1:-/destination/path}"
+if [ ! -d "$dest" ]; then
+    echo "$dest not found"
+    exit 1
 fi
 
-dest=/run/media/USER/EXTERNAL_HD2
-if [ -d $dest ]; then
-	rsync ${rsyncopts} ${HOME}/calibre/ ${dest}/calibre
-	rsync ${rsyncopts} ${HOME}/configs/ ${dest}/configs
-	rsync ${rsyncopts} ${HOME}/dev/ ${dest}/dev
-	rsync ${rsyncopts} ${HOME}/Documents/ ${dest}/doc
-	rsync ${rsyncopts} ${HOME}/games/ ${dest}/games
-	rsync ${rsyncopts} ${HOME}/Pictures/ ${dest}/img
-	rsync ${rsyncopts} ${HOME}/Music/library/ ${dest}/music
-	rsync ${rsyncopts} ${HOME}/Music/sfx/ ${dest}/music-sfx
-	rsync ${rsyncopts} ${HOME}/Music/flac/ ${dest}/music-flac
-	rsync ${rsyncopts} ${HOME}/org/ ${dest}/org
-	rsync ${rsyncopts} ${HOME}/prj/ ${dest}/prj
-	rsync ${rsyncopts} ${HOME}/sw/ ${dest}/sw
-fi
+dirs=(
+    "${HOME}/3d-printing/" "${dest}/3d-printing"
+    "${HOME}/Documents/" "${dest}/doc"
+    "${HOME}/Mail/" "${dest}/mail"
+    "${HOME}/Music/flac/" "${dest}/music-flac"
+    "${HOME}/Music/library/" "${dest}/music"
+    "${HOME}/Music/playlists/" "${dest}/music-playlists"
+    "${HOME}/Music/sfx/" "${dest}/music-sfx"
+    "${HOME}/News/" "${dest}/news"
+    "${HOME}/Pictures/" "${dest}/img"
+    "${HOME}/calibre/" "${dest}/calibre"
+    "${HOME}/configs/" "${dest}/configs"
+    "${HOME}/dev/" "${dest}/dev"
+    "${HOME}/foo-shop/" "${dest}/foo-shop"
+    "${HOME}/games/" "${dest}/games"
+    "${HOME}/org-backups/" "${dest}/org-backups"
+    "${HOME}/org/" "${dest}/org"
+    "${HOME}/prj/" "${dest}/prj"
+    "${HOME}/sw/" "${dest}/sw"
+)
+
+while (( ${#dirs[@]} )); do
+    src="${dirs[0]}"
+    dst="${dirs[1]}"
+    echo -e "\n\e[1;34mBacking up ${src} => ${dst}\e[0m"
+    rsync ${rsyncopts} ${src} ${dst}
+
+    dirs=( ${dirs[@]:2} )
+done
